34552a512db00888367353fd2a6e910a6011bfc5
Conner Petzold <cpetzold@gmail.com>
2013-09-28 18:55:20 -0700

    refine random word

--- src/clj/words/server.clj
+++ src/clj/words/server.clj
@@ -9,21 +9,38 @@
    [hiccup.page :as page]
    [hiccup.element :as element]
    [wordnik.core :as wn]
+   [wordnik.api.word :as wn-word]
    [wordnik.api.words :as wn-words]
    [digest :as digest]
    [words.css :as css]))
 
-(def wordnik-key "997ea3a64b190c6d8f0040df7b6003393e51bbd224bc5ec8d")
+(def +wordnik-key+ "997ea3a64b190c6d8f0040df7b6003393e51bbd224bc5ec8d")
+(def +excluded-pos+ ["family-name"
+                     "given-name"
+                     "noun-plural"
+                     "noun-posessive"
+                     "proper-noun"
+                     "proper-noun-plural"
+                     "proper-noun-posessive"])
 
 (defn scramble [s]
   (->> (seq s) shuffle (apply str)))
 
 (defn scrambled-word []
-  (wn/with-api-key wordnik-key
-    (let [word (:word (wn-words/random-word :maxLength 6 :minCorpusCount 5000))]
-      {:word word
-       :scrambled-word (scramble word)
-       :encoded-word (digest/md5 word)})))
+  (wn/with-api-key +wordnik-key+
+    (let [word (:word (wn-words/random-word
+                       :maxLength 6
+                       :minCorpusCount 5000
+                       :excludePartOfSpeech (str/join "," +excluded-pos+)))
+          parts-of-speech (->> (wn-word/definitions word)
+                               (map :partOfSpeech)
+                               (filter identity))]
+      (println word (count parts-of-speech))
+      (if-not (seq parts-of-speech)
+        (scrambled-word)
+        {:word word
+         :scrambled-word (scramble word)
+         :encoded-word (digest/md5 word)}))))
 
 (defn layout [& content]
   (page/html5
